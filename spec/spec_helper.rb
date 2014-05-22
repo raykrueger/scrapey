b95b97408ed4df222b226f1887c46c17304c1d7c
Ray Krueger <raykrueger@gmail.com>
2014-05-22 09:35:31 -0500

    Restore local simplecov output.
    Coveralls took it away.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
-require 'coveralls' ; Coveralls.wear! 
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
+# All other 'requires' must come in after simplecov
+
 require 'scrapey'
 require 'webmock/rspec'
 
